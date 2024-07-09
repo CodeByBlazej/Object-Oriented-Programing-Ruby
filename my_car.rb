e6fc144fb98a154779255f5195fe13a55eb038f9
CodeByBlazej <codebyblazej@gmail.com>
2024-07-09 19:18:25 +0100

    add more methods

--- my_car.rb
+++ my_car.rb
@@ -1,30 +1,63 @@
 class MyCar
 
-  attr_accessor :year, :color, :model
-
+  attr_accessor :color, :model, :speed
+  attr_reader :year
 
   def initialize(y, c, m)
     @year = y
     @color = c
     @model = m 
+    @speed = 0
+  end
+
+  def change_color(c)
+    self.color = c 
+  end
+
+  def car_info
+    "It is #{model} produced in #{year} and it's color is #{color}"
   end
 
   def start
     puts "Engine starting..."
-    @speed = s
   end
 
   def stop
+    @speed = 0
     puts "Engine stops..."
   end
 
-  def accelerate
-    "#{speed + 10}"
+  def accelerate(number)
+    @speed += number
+    puts "You push the gas and accelerate #{number} mph"
+  end
+
+  def break(number)
+    @speed -= number
+    puts "You press the break and decelerate #{number} mph"
+  end
+
+  def current_speed
+    puts "Your current speed is #{speed}"
   end
 
 
 end
 
 bmw = MyCar.new("2010", "red", "e90")
+puts bmw.car_info
+bmw.change_color("blue")
+puts bmw.car_info
+puts bmw.color
+puts bmw.year
 bmw.start
 bmw.stop
+bmw.start
+bmw.accelerate(10)
+bmw.current_speed
+bmw.accelerate(20)
+bmw.current_speed
+bmw.break(5)
+bmw.current_speed
+bmw.stop
+bmw.current_speed
