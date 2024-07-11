4343e5d47819c959534e9d1685afc67600bb2c2c
CodeByBlazej <codebyblazej@gmail.com>
2024-07-11 19:16:32 +0100

    add class with to_s method

---
+++ good_dog3.rb
@@ -0,0 +1,22 @@
+class GoodDog3
+
+  DOG_YEARS = 7
+
+  attr_accessor :name, :age
+
+  def initialize(n, a)
+    @name = n
+    @age = a * DOG_YEARS
+  end
+
+  def to_s
+    "This dog's name is #{name} and it is #{age} in dog years."
+  end
+
+end
+
+sparky = GoodDog3.new('Sparky', 4)
+
+puts sparky
+
+p sparky
