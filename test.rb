8c67894061b0127285e53aa0e7e62664cda05f08
CodeByBlazej <codebyblazej@gmail.com>
2024-07-25 20:08:01 +0100

    rescue StandardError

--- test.rb
+++ test.rb
@@ -2,8 +2,10 @@ while 1
   puts "Enter a number>>"
   begin
     num = Kernel.gets.match(/\d+/)[0]
-  rescue
+  rescue StandardError=>e
     puts "Erroneud input! Try again..."
+    puts e
+    puts "\Try again...\n"
   else
     puts "#{num} + 1 is: #{num.to_i+1}"
   end
