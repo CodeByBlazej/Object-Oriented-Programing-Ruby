137eec106d45c1e660f4db236f6305865fb1c099
CodeByBlazej <codebyblazej@gmail.com>
2024-07-26 18:28:17 +0100

    add to_fahrenheit method

--- test2.rb
+++ test2.rb
@@ -2,4 +2,12 @@ class Celsius
   def initialize(temperature)
     @temperature = temperature
   end
+
+  def to_fahrenheit()
+    @temperature * 1.8 + 32
+  end
 end
+
+celsius = Celsius.new(10)
+
+puts celsius.to_fahrenheit
