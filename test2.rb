627c80a67c2677c2cbcfac347d20d374d609113c
CodeByBlazej <codebyblazej@gmail.com>
2024-07-28 20:28:22 +0100

    add solution to the last quiz

--- test2.rb
+++ test2.rb
@@ -1,23 +1,17 @@
-class Person
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
-
-  def first_name()
-    return(@first_name)
-  end
-
-  def last_name()
-    return(@last_name)
-  end
-
-  def full_name()
-    return("#{@first_name} #{@last_name}")
+module MathHelpers
+  def exponent(num1, num2)
+    num1 ** num2
   end
 end
 
-p = Person.new("Blazej", "Magiera")
-puts p.first_name
-puts p.last_name
-puts p.full_name
+class Calculator
+  include MathHelpers
+
+  def squate_root(num1)
+    return(exponent(num1, 0.5))
+  end
+end
+
+n = Calculator.new
+puts n.squate_root(2)
+
