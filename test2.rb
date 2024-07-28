14024205832e5cffd297dda1409107dbf5bb25a8
CodeByBlazej <codebyblazej@gmail.com>
2024-07-28 20:00:32 +0100

    solve another quiz

--- test2.rb
+++ test2.rb
@@ -1,11 +1,21 @@
-class Mammal
-  def heartbeat?
-    true
+class BaseballPlayer
+  def initialize(hits, walks, at_bats)
+    @hits = hits
+    @walks = walks
+    @at_bats = at_bats
+  end
+
+  def battling_average()
+    return(@hits.to_f / @at_bats)
+  end
+
+  def on_base_percentage()
+    return((@hits + @walks).to_f / @at_bats)
   end
 end
 
-class Dog < Mammal
-end
+player = BaseballPlayer.new(330, 110, 1125)
 
-d = Dog.new()
-puts d.heartbeat?
+puts player.battling_average
+puts player.on_base_percentage
+
