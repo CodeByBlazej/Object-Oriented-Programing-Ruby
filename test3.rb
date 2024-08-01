2fdcebc6d0fda7c1864ad6f0dae02b9e9705a2ac
CodeByBlazej <codebyblazej@gmail.com>
2024-08-01 18:31:04 +0100

    add class constant and call it with ::

--- test3.rb
+++ test3.rb
@@ -1,34 +1,5 @@
-class Base
-  def initialize
-    @name = "Base"
-  end
-
-  private
-
-  def private_method
-    puts "private method called"
-  end
-
-  protected
-
-  def protected_method
-    puts "protected method called"
-  end
-
-  public
-
-  def get_name
-    return @name
-  end
+class MMath
+  PI = 3.141592
 end
 
-class Derived < Base
-  def public_method
-    private_method
-    protected_method
-  end
-end
-
-d = Derived.new
-d.public_method
-puts d.get_name
+puts MMath::PI
